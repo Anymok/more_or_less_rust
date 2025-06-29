8c4b68416c6f01900ce917e5ae4ceb0a7a30e836
Anymok <secure-anymok@proton.me>
2025-06-29 14:17:14 +0200

    game finished

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::io;
+use std::cmp::Ordering;
+use rand;
+
 fn main() {
-    println!("Hello, world!");
+    let nombre_secret = rand::random_range(0..=100);
+
+    println!("Le nombre secret est : {}", nombre_secret);
+
+    println!("Devinez le nombre !");
+
+    loop {
+        let mut supposition = String::new();
+
+        io::stdin()
+            .read_line(&mut supposition)
+            .expect("Echec de la lecteur de l'entrée de l'utilisateur.");
+
+        let supposition: u32 = match supposition.trim().parse() {
+            Ok(nombre) => nombre,
+            Err(_) => continue,
+        };
+
+        println!("Votre nombre : {}", supposition);
+
+        match supposition.cmp(&nombre_secret) {
+            Ordering::Less => println!("Le nombre secret est plus grand."),
+            Ordering::Greater => println!("Le nombre secret est plus petit"),
+            Ordering::Equal => {
+                println!("Vous avez gagné !");
+                break;
+            },
+        }
+        println!("Veuillez saisir un nombre.");
+    }    
 }
